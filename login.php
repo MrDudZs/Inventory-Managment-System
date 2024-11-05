934dad567306a1bf3907c73c305a3afb5fa64d2a
Thalia <148969711+TZevs@users.noreply.github.com>
2024-11-05 12:27:01 +0000

    Frontend login

--- login.php
+++ login.php
@@ -1,49 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
+        <link href="bootstrap-5.3.3-dist/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="css/main.css" rel="stylesheet" />
+    </head>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link href="css/main.css" rel="stylesheet" />
-    <link href="bootstrap-5.3.3-dist/css/bootstrap.min.css" rel="stylesheet" />
-</head>
+    <body>
+        <?php
 
-<body>
-    <div class="contaienr">
-        <div class="header">
-            <header>INV MGMT SYSTEM</header>
-        </div>
-        <div class="navbar">
-            <nav></nav>
-        </div>
-        <div class="main">
-            <main>
-                <div class="container-sm">
-                    <form action="php/Login/loginProcess.php" method="post">
-                        <input class="loginInput" type="text" placeholder="Email" name="email" required>
-                        <br />
-                        <input class="loginInput" type="password" placeholder="password" name="password" required>
-                        <br />
-                        <input class="loginBtn" type="submit" value="Login">
-                    </form>
-                    <div class="formPasword">
-                        <form action="php/Register/passwordResetProcess.php" method="post">
-                            <br />
-                            <input class="loginInput" type="email" name="emailRest" placeholder="email">
-                            <br />
-                            <input class="loginInput" type="password" name="passwordReset1" placeholder="Rest Password">
-                            <br />
-                            <input class="loginInput" type="password" name="passwordReset2" placeholder="Rest Password">
-                            <br />
-                            <input class="loginBtn" type="submit" value="Reset Password">
-                        </form>
-                    </div>
-                </div>
-            </main>
-        </div>
-    </div>
-    </div>
-</body>
-
+        ?>
+        <main>
+            <br><br>
+            <div class="container-sm container-custom">
+                <h1>LOGIN</h1>
+                <hr>
+                <form action="php/Login/loginProcess.php" method="post">
+                    <input class="form-control" id="exampleFormControlInput1" type="email" placeholder="Username" name="email" required>
+                    <br />
+                    <input class="form-control" id="inputPassword5" type="password" placeholder="Password" name="password" required>
+                    <button class="btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"><u>Forgotten Password?</u></button>
+                    <br />
+                    <input class="btn btn-custom" type="submit" value="LOGIN">
+                </form>
+            </div>
+            <br>
+            <div class="collapse container-sm container-custom" id="collapseExample">
+                <h2>Reset Password</h2>
+                <hr>
+                <form action="php/Register/passwordResetProcess.php" method="post">
+                    <input class="form-control" type="email" name="emailRest" placeholder="Username">
+                    <br />
+                    <input class="form-control" type="password" name="passwordReset1" placeholder="New Password">
+                    <br />
+                    <input class="form-control" type="password" name="passwordReset2" placeholder="Confrim New Password">
+                    <br />
+                    <input class="btn btn-custom" type="submit" value="Reset Password">
+                </form>
+            </div>
+            <br><br>
+        </main>
+    </body>
+    <script src="bootstrap-5.3.3-dist/js/bootstrap.js"></script>
 </html>
