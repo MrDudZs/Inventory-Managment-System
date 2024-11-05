f59914654d92718d40c06967def3500f72d2643c
Thalia <148969711+TZevs@users.noreply.github.com>
2024-11-05 13:30:08 +0000

    Ready for php

--- dashboard.php
+++ dashboard.php
@@ -1,13 +1,26 @@
+<?php
+    
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Dashboard</title>
+    <link href="bootstrap-5.3.3-dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="css/main.css" rel="stylesheet" />
 
 </head>
-<body>
-    
-</body>
+    <body>
+        <?php
+
+        ?>
+
+        <main>
+            <?php
+
+            ?>
+        </main>
+    </body>
+    <script src="bootstrap-5.3.3-dist/js/bootstrap.js"></script>
 </html>
