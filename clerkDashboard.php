8938c142a0489014654ff22457ec8c7828ca5b9c
Sovietsupporter69 (laptop) <84580170+Sovietsupporter69@users.noreply.github.com>
2024-11-06 20:46:15 +0000

    Added the product sold
    Added the total product sold in money, the rest of this page is links so need to wait for the relevant page to be made, though I added temporary links for now and the categories link works

--- clerkDashboard.php
+++ clerkDashboard.php
@@ -9,16 +9,16 @@
             <h4>Invoices:</h4>
             <hr>
             <div class="d-grid gap-2">
-                <button type="button" class="btn-dashboard">Create Invoice</button>
-                <button type="button" class="btn-dashboard">Invoice History</button>
+                <button type="button" class="btn-dashboard" onclick='window.location.href = "createInvoice.php"'>Create Invoice</button>
+                <button type="button" class="btn-dashboard" onclick='window.location.href = "invoiceHistory.php"'>Invoice History</button>
             </div>
         </div>
         <div class="col custom-dash-cols">
         <h4>Products:</h4>
             <hr>
             <div class="d-grid gap-2">
-                <button type="button" class="btn-dashboard">Categories</button>
-                <button type="button" class="btn-dashboard">Search</button>
+                <button type="button" class="btn-dashboard" onclick='window.location.href = "categories.php"'>Categories</button>
+                <button type="button" class="btn-dashboard" onclick='window.location.href = "search.php"'>Search</button>
             </div>
         </div>
     </div>
@@ -27,15 +27,13 @@
             <h4>This Week:</h4>
             <hr>
             <div class="stockStats">
-                <p class="productSoldStat">Product Sold:</p>
-                <p class="averageStockStat">Avg Stock:</p>
+                <p class="productSoldStat">Product Sold:<?php GetSales("1 week") ?></p> 
             </div>
             <hr>
             <h4>This Month:</h4>
             <hr>
             <div class="stockStats">
-                <p class="productSoldStat">Product Sold:</p>
-                <p class="averageStockStat">Avg Stock:</p>
+                <p class="productSoldStat">Product Sold:<?php GetSales("1 month") ?></p>
             </div>  
         </div>
     </div>
@@ -52,6 +50,24 @@
             while ($row = $searchResults->fetch_assoc()){
                 echo $row['stockBrand'] . ", " . $row['stockName'] . ". Stock left: " . $row['stockCount'] . "<br>";
             }
+        }
+        $conn->close();
+    }
+
+    function GetSales($timeScale) {
+        include 'php/DB-Connection/configDB.php';
+        
+        $currentDate = date('Ymd', strtotime("Now"));
+        $lastWeeksDate = date('Ymd', strtotime("-" . $timeScale));
+        $sql = "SELECT invoiceCost FROM invoice WHERE invoiceDate < " . $currentDate . " AND invoiceDate > " . $lastWeeksDate;
+        $searchResults = $conn->query($sql);
+
+        $cumulativeSales = 0;
+        if ($searchResults->num_rows > 0){
+            while ($row = $searchResults->fetch_assoc()){
+                $cumulativeSales += $row['invoiceCost'];
+            }
+            echo " £" . $cumulativeSales;
         }
         $conn->close();
     }
