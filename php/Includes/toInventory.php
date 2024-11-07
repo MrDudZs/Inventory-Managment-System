42621ca384c6c677dd26d8d85d00b2152e1a7d93
Thalia <148969711+TZevs@users.noreply.github.com>
2024-11-07 15:11:39 +0000

    commented out the db query and output.

--- php/Includes/toInventory.php
+++ php/Includes/toInventory.php
@@ -3,41 +3,39 @@
     include '../DB-Connection/configDB.php';
 
     $selected_category = $_POST['category'];    
-    // header("Location: ../../inventory.php");
-    // exit();
 
-    if ($selected_category == "allProducts") {
-        $stmt = $conn->prepare("SELECT `productName`, `productManufacture`, `productType`, `productCount` FROM `products`");
-        $stmt->execute();
-        $allStock = $stmt->get_result();
+    // if ($selected_category == "allProducts") {
+    //     $stmt = $conn->prepare("SELECT `productName`, `productManufacture`, `productType`, `productCount` FROM `products`");
+    //     $stmt->execute();
+    //     $allStock = $stmt->get_result();
 
-        if($allStock->num_row >= 1) {
-            $products = $allStock->fetchAll(PDO::FETCH_ASSOC);
+    //     if($allStock->num_row >= 1) {
+    //         $products = $allStock->fetchAll(PDO::FETCH_ASSOC);
             
-            $_SESSION['product_stock'] = $products; 
-            $_SESSION['product_category'] = $selected_category;
+    //         $_SESSION['product_stock'] = $products; 
+    //         $_SESSION['product_category'] = $selected_category;
     
-            header("Location: ../../inventory.php");
-            exit();
-        } else {
-            echo "No Products Available.";
-        }
-    } else {
-        $stmt = $conn->prepare("SELECT `productName`, `productManufacture`, `productType`, `productCount` FROM `products` WHERE productType=?");
-        $stmt->bind_param("s", $selected_category);
-        $stmt->execute();
-        $resultStock = $stmt->get_result();
+    //         header("Location: ../../inventory.php");
+    //         exit();
+    //     } else {
+    //         echo "No Products Available.";
+    //     }
+    // } else {
+    //     $stmt = $conn->prepare("SELECT `productName`, `productManufacture`, `productType`, `productCount` FROM `products` WHERE productType=?");
+    //     $stmt->bind_param("s", $selected_category);
+    //     $stmt->execute();
+    //     $resultStock = $stmt->get_result();
 
-        if($resultStock->num_row >= 1) {
-            $products = $resultStock->fetchAll(PDO::FETCH_ASSOC);
+    //     if($resultStock->num_row >= 1) {
+    //         $products = $resultStock->fetchAll(PDO::FETCH_ASSOC);
             
-            $_SESSION['product_stock'] = $products; 
-            $_SESSION['product_category'] = $selected_category;
+    //         $_SESSION['product_stock'] = $products; 
+    //         $_SESSION['product_category'] = $selected_category;
     
-            header("Location: ../../inventory.php");
-            exit();
-        } else {
-            echo "Category '$selected_category' doesn't exist.";
-        }
-    }
+    //         header("Location: ../../inventory.php");
+    //         exit();
+    //     } else {
+    //         echo "Category '$selected_category' doesn't exist.";
+    //     }
+    // }
 ?>
