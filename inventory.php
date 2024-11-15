ef9e501d0722a6984f582c47cbe3083765c6b1a3
Thalia <148969711+TZevs@users.noreply.github.com>
2024-11-15 15:28:28 +0000

    Include for query. Default output.

--- inventory.php
+++ inventory.php
@@ -1,26 +1,10 @@
-<?php
-    include('php/DB-Connection/configDB.php');
-
-    $s_category = $_POST['category'];
-    
-    if ($s_category == "allProducts") {
-        $stmt = $conn->prepare("SELECT `stockID`, `stockName`, `stockCount`, `stockType`, `stockPrice`, `stockBrand` FROM `stock`");
-        $stmt->execute();
-        $allStock = $stmt->get_result();
-
-    } else {
-        $stmt = $conn->prepare("SELECT `stockID`, `stockName`, `stockCount`, `stockType`, `stockPrice`, `stockBrand` FROM `stock` WHERE stockType=?");
-        $stmt->bind_param("s", $s_category);
-        $stmt->execute();
-        $resultStock = $stmt->get_result();
-    }
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inventory</title>
+    <link href="bootstrap-5.3.3-dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="css/main.css" rel="stylesheet" />
 </head>
     <body>
@@ -35,33 +19,29 @@
                 <br><br>
                 <div class="row">
                     <?php
-                        if (isset($allStock) && $allStock->num_rows > 0) {
-                            echo "<h2 class=\"btn-custom\" style=\"padding: 7px;\">All Products</h2>";
-                            while ($row = $allStock->fetch_assoc()) {
+                        include "php/Includes/toInventory.php";
+
+                        // If is null then allProducts is default.
+                        $category = isset($_POST['category']) ? $_POST['category'] : 'allProducts';
+                        $inventory = fetchInventory($category);
+
+                        if (isset($inventory)) {
+                            if ($category == "allProducts") {
+                                echo "<h2 class=\"btn-custom\" style=\"padding: 7px;\">All Products</h2>";
+                            } else {
+                                echo "<h2 class=\"btn-custom\" style=\"padding: 7px;\">Product Category: " . $category . "</h2>";
+                            }
+                            // $item[index] - Index from the db column order
+                            foreach ($inventory as $item) {
                                 echo "<div class=\"col-md-4 mb-4\">";
                                     echo "<div class=\"card grid-sizing\" style=\"width: 18rem; height: 18rem;\">";
                                         echo "<div class=\"card-body\">";
-                                            echo "<h5 class=\"card-title\">" . $row['stockName'] . "</h5>";
-                                            echo "<h6 class=\"card-subtitle mb-2 text-body-secondary\">StockID: " . $row['stockID'] . "</h6>";
-                                            echo "<p class=\"card-text\">Category: " . $row['stockType'] . "</p>";
-                                            echo "<p class=\"card-text\">Brand: " . $row['stockBrand'] . "</p>";
-                                            echo "<p class=\"card-text\">Price: £" . $row['stockPrice'] . "</p>";
-                                            echo "<p class=\"card-text\">" . $row['stockCount'] . " - In Stock</p>";
-                                        echo "</div>";
-                                    echo "</div>";
-                                echo "</div>";
-                            }
-                        } else if (isset($resultStock) && $resultStock->num_rows > 0) {
-                            echo "<h2 class=\"btn-custom\" style=\"padding: 7px;\">Product Category: " . $s_category . "</h2>";
-                            while ($row = $resultStock->fetch_assoc()) {
-                                echo "<div class=\"col-md-4 mb-4\">";
-                                    echo "<div class=\"card\" style=\"width: 20rem; height: 18rem;\">";
-                                        echo "<div class=\"card-body\">";
-                                            echo "<h5 class=\"card-title\">" . $row['stockName'] . "</h5>";
-                                            echo "<h6 class=\"card-subtitle mb-2 text-body-secondary\">StockID: " . $row['stockID'] . "</h6>";
-                                            echo "<p class=\"card-text\">Brand: " . $row['stockBrand'] . "</p>";
-                                            echo "<p class=\"card-text\">Price: £" . $row['stockPrice'] . "</p>";
-                                            echo "<p class=\"card-text\">" . $row['stockCount'] . "- In Stock</p>";
+                                            echo "<h5 class=\"card-title\">" . $item[1] . "</h5>";
+                                            echo "<h6 class=\"card-subtitle mb-2 text-body-secondary\">StockID: " . $item[0] . "</h6>";
+                                            echo "<p class=\"card-text\">Category: " . $item[3] . "</p>";
+                                            echo "<p class=\"card-text\">Brand: " . $item[5] . "</p>";
+                                            echo "<p class=\"card-text\">Price: £" . $item[4] . "</p>";
+                                            echo "<p class=\"card-text\">" . $item[2] . " - In Stock</p>";
                                         echo "</div>";
                                     echo "</div>";
                                 echo "</div>";
