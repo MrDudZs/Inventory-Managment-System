bdebc0dc4316ba92f68ae740b497b0e51a98f8cc
Thalia <148969711+TZevs@users.noreply.github.com>
2024-11-30 14:18:24 +0000

    saw on a tutorial

--- public/index.php
+++ public/index.php
@@ -1,3 +1,22 @@
+<?php
+// // Saw this in a tutorial not sure if its needed or not.
+// use Illuminate\Http\Request;
+
+// define('LARAVEL_START', microtime(true));
+
+// // Determine if the application is in maintenance mode...
+// if(file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+//     require $maintenance;
+// }
+
+// // Register the Composer autoloader...
+// require __DIR__.'/../vendor/autoload.php';
+
+// // Boostrap Laravel and handle the request...
+// (require_once __DIR__.'/../bootstrap/app.php')
+//     ->handleRequest(Request::capture());
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
