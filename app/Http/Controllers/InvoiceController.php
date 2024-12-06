e29986e1259c7b2d55b82baa84fe01db75a5243b
Thalia <148969711+TZevs@users.noreply.github.com>
2024-12-06 17:10:36 +0000

    functions for handling form empty. And fetching dropdown data.

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -4,11 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Stock;
 
 class InvoiceController extends Controller
 {
     public function create() 
     {
         return view('createInvoice');
+    }
+
+    public function handleForm(Request $request) 
+    {
+
+    }
+
+    public function fetchData(Request $request) 
+    {
+        $value = $request->input('value');
+        $data = Stock::where('stockType', $value)->select('stockId', 'stockName')->get();
+
+        return response()->json($data);
     }
 }
