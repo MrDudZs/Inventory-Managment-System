c2e4009893d4c9c00bf117f67ee7623f6e8a1002
Matt <123038044+MrDudZs@users.noreply.github.com>
2024-12-22 13:51:41 +0000

    Debug clerkDashboard.blade.php
    Debugging to see if redirect of dashboard works when removing invalid errors

--- resources/views/Includes/clerkDashboard.blade.php
+++ resources/views/Includes/clerkDashboard.blade.php
@@ -3,7 +3,7 @@
         <div class="col custom-dash-cols">
             <h4>Low Stock:</h4>
             <hr>
-            @foreach ($lowStocks as $stock)
+                        @foreach ($lowStocks as $stock)
                 <p>{{ $stock->stockBrand }}, {{ $stock->stockName }}. Stock left: {{ $stock->stockCount }}</p>
             @endforeach
         </div>
@@ -11,16 +11,20 @@
             <h4>Invoices:</h4>
             <hr>
             <div class="d-grid gap-2">
-                <button type="button" class="btn-dashboard" onclick='window.location.href = "{{ url('createInvoice') }}"'>Create Invoice</button>
-                <button type="button" class="btn-dashboard" onclick='window.location.href = "{{ url('invoiceHistory') }}"'>Invoice History</button>
+                <button type="button" class="btn-dashboard"
+                    onclick='window.location.href = "{{ url('createInvoice') }}"'>Create Invoice</button>
+                <button type="button" class="btn-dashboard"
+                    onclick='window.location.href = "{{ url('invoiceHistory') }}"'>Invoice History</button>
             </div>
         </div>
         <div class="col custom-dash-cols">
             <h4>Products:</h4>
             <hr>
             <div class="d-grid gap-2">
-                <button type="button" class="btn-dashboard" onclick='window.location.href = "{{ url('categories') }}"'>Categories</button>
-                <button type="button" class="btn-dashboard" onclick='window.location.href = "{{ url('search') }}"'>Search</button>
+                <button type="button" class="btn-dashboard"
+                    onclick='window.location.href = "{{ url('categories') }}"'>Categories</button>
+                <button type="button" class="btn-dashboard"
+                    onclick='window.location.href = "{{ url('search') }}"'>Search</button>
             </div>
         </div>
     </div>
@@ -29,13 +33,13 @@
             <h4>This Week:</h4>
             <hr>
             <div class="stockStats">
-                <p class="productSoldStat">Product Sold: £{{ $salesWeek }}</p>
+                <!-- <p class="productSoldStat">Product Sold: £{{ $salesWeek }}</p> -->
             </div>
             <hr>
             <h4>This Month:</h4>
             <hr>
             <div class="stockStats">
-                <p class="productSoldStat">Product Sold: £{{ $salesMonth }}</p>
+                <!--<p class="productSoldStat">Product Sold: £{{ $salesMonth }}</p> -->
             </div>
         </div>
     </div>
